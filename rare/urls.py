9a0ed5801c196aa274fa7bc1a3957e4192b1d6e5
brianminges <btminges@gmail.com>
2022-07-06 18:22:19 -0500

    tix 24 ready to test

--- rare/urls.py
+++ rare/urls.py
@@ -19,8 +19,10 @@ from django.urls import path
 from django.conf.urls import include
 from rareapi.views import register_user, login_user
 from rest_framework import routers
+from rareapi.views import PostView
 
 router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'posts', PostView, 'post')
 
 urlpatterns = [
     path('register', register_user),
