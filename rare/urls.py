a9a7430a1050d8ee3ebf9d9d3e428f5e806e2c7e
brianminges <btminges@gmail.com>
2022-07-06 22:22:26 -0500

    bm-18 create post form functions

--- rare/urls.py
+++ rare/urls.py
@@ -19,10 +19,12 @@ from django.urls import path
 from django.conf.urls import include
 from rareapi.views import register_user, login_user
 from rest_framework import routers
-from rareapi.views import PostView
+from rareapi.views import PostView, CategoryView
+
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'posts', PostView, 'post')
+router.register(r'categories', CategoryView, 'category')
 
 urlpatterns = [
     path('register', register_user),
--- rareapi/views/post.py
+++ rareapi/views/post.py
@@ -1,11 +1,10 @@
 """View module for handling requests about posts"""
-from unicodedata import category
 from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
 from django.core.exceptions import ValidationError
-from rareapi.models import Post
+from rareapi.models import Post, RareUser
 
 class PostView(ViewSet):
     """Rare posts view"""
@@ -21,10 +20,30 @@ class PostView(ViewSet):
         serializer = PostSerializer(posts, many=True)
         return Response(serializer.data)
     
+    def create(self, request):
+        """Handle POST operations
+        
+        Returns:
+            Response -- JSON serialized post instance
+        """
+        user = RareUser.objects.get(user=request.auth.user)
+        serializer = CreatePostSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(user=user)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+        
 class PostSerializer(serializers.ModelSerializer):
     """JSON serializer for posts
     """
     class Meta:
         model = Post
-        fields = ('id', 'user', 'category', 'title', 'publication_date', 'image_url', 'content', 'tags', 'reactions')
+        fields = ['id', 'user', 'category', 'title', 'publication_date', 'image_url', 'content', 'tags', 'reactions']
         depth = 2
+
+class CreatePostSerializer(serializers.ModelSerializer):
+    """JSON serializer for posts
+    """
+    class Meta:
+        model = Post
+        fields = ['id', 'category', 'title', 'publication_date', 'image_url', 'content', 'approved']
