aaedaa691755191ca4daed341326694b210fcd13
brianminges <btminges@gmail.com>
2022-07-09 13:35:49 -0500

    early comments form

--- rareapi/views/comment.py
+++ rareapi/views/comment.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework import serializers, status
 from rest_framework.decorators import action
 from django.core.exceptions import ValidationError
-from rareapi.models import Comment
+from rareapi.models import Comment, RareUser
 
 class CommentView(ViewSet):
     """Rare comments view"""
@@ -19,6 +19,19 @@ class CommentView(ViewSet):
         comments = Comment.objects.all()
         serializer = CommentSerializer(comments, many=True)
         return Response(serializer.data)
+    
+    def create(self, request):
+        """Handle POST operations
+        
+        Returns:
+            Response -- JSON serialized post instance
+        """
+        
+        user = RareUser.objects.get(user=request.auth.user)
+        serializer = CreateCommentSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(author=user)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
      
 class CommentSerializer(serializers.ModelSerializer):
     """JSON serializer for comments
@@ -28,3 +41,10 @@ class CommentSerializer(serializers.ModelSerializer):
         model = Comment
         fields = ['id', 'post', 'author', 'content', 'created_on']
         depth = 3
+        
+class CreateCommentSerializer(serializers.ModelSerializer):
+    """JSON serializer for comments
+    """
+    class Meta:
+        model = Comment
+        fields = ['id', 'post', 'author', 'content', 'created_on']
