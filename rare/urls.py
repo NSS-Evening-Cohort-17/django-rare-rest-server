fb91c36eb67d869060f33ff6d390f8b55201191f
brianminges <btminges@gmail.com>
2022-07-09 11:09:44 -0500

    bm-6 view post's comments functions

--- rare/urls.py
+++ rare/urls.py
@@ -19,12 +19,13 @@ from django.urls import path
 from django.conf.urls import include
 from rareapi.views import register_user, login_user
 from rest_framework import routers
-from rareapi.views import PostView, CategoryView
+from rareapi.views import PostView, CategoryView, CommentView
 
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'posts', PostView, 'post')
 router.register(r'categories', CategoryView, 'category')
+router.register(r'comments', CommentView, 'comment')
 
 urlpatterns = [
     path('register', register_user),
